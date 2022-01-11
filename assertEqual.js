b017e8970206ea02aeff9dd9a7e1e4e309e44004
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-01-10 16:02:33 -0800

    create assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log("🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected);
+  } else {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
