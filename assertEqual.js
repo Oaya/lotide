f18a14a3843b98730dfe76c962dd1f658fe41507
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-01-11 00:41:05 +0000

    run eslint

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log("🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
   } else {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
