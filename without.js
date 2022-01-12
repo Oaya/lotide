4c29087d96d1316f8c20d8d92834078d1cd68de5
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-01-12 04:11:28 +0000

    create without function

---
+++ without.js
@@ -0,0 +1,48 @@
+const assertArraysEqual = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    console.log(`🔴🔴🔴 Comparison Failed: ${arr1} !== ${arr2}`);
+    return false;
+  }
+  if (arr1 === [] || arr2 === []) {
+    console.log(`🔴🔴🔴 Comparison Failed: ${arr1} !== ${arr2}`);
+    return false;
+  }
+  let i = arr1.length;
+  while (i--) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`🔴🔴🔴 Comparison Failed: ${arr1} !== ${arr2}`);
+      return false;
+    } else {
+      console.log(`✅✅✅ Comparison Passed: ${arr1} === ${arr2}`);
+      return true;
+    }
+  }
+};
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  if (arr1 === [] || arr2 === []) {
+    return false;
+  }
+  let i = arr1.length;
+  while (i--) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+const without = (source, itemToRemove) => {
+  for (let i = 0; source.length > i; i++) {
+    for (let j = 0; itemToRemove.length > j; j++) {
+      if (source[i] === itemToRemove[j]) {
+        source.splice(i, 1);
+      }
+    }
+  }
+  return source;
+};
