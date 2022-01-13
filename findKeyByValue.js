44f9680d0e3f2f0bf2172b8d29c7a493a4ed5d4c
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-01-13 01:47:14 +0000

    Implement letterPositions

---
+++ findKeyByValue.js
@@ -0,0 +1,17 @@
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+const findKeyByValue = (obj, val) => {};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
---
+++ letterPositions.js
@@ -0,0 +1,60 @@
+const assertArraysEqual = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    console.log(`🔴🔴🔴 Comparison Failed: ${arr1} !== ${arr2}`);
+    return false;
+  }
+  if (arr1 === [] || arr2 === []) {
+    console.log(`🔴🔴🔴 Comparison Failed: ${arr1} !== ${arr2}`);
+    return false;
+  }
+  let i = arr1.length;
+  while (i--) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`🔴🔴🔴 Comparison Failed: ${arr1} !== ${arr2}`);
+      return false;
+    } else {
+      console.log(`✅✅✅ Comparison Passed: ${arr1} === ${arr2}`);
+      return true;
+    }
+  }
+};
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  if (arr1 === [] || arr2 === []) {
+    return false;
+  }
+  let i = arr1.length;
+  while (i--) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+const letterPositions = (sentence) => {
+  const result = {};
+  //go through the sentence//
+  for (let i = 0; sentence.length > i; i++) {
+    //if the sentence[i] is space not check//
+    if (sentence[i] === " ") {
+      result;
+    }
+    //check if sentence[i] is exsits in the result//
+    else if (!result[sentence[i]]) {
+      result[sentence[i]] = [i];
+    } else {
+      result[sentence[i]].push(i);
+    }
+  }
+
+  return result;
+};
+
+letterPositions("lighthouse in the house");
+assertArraysEqual(letterPositions("lighthouse in the house").e, [9, 16, 22]);
+console.log(eqArrays(letterPositions("hello").e, [1]));
