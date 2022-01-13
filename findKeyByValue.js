608d967c1db9dafe8794e55e6b67f3f7efbb31ef
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-01-13 02:10:42 +0000

    Implement findKeyByValue in Lotide

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,13 +5,23 @@ const assertEqual = function (actual, expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
 };
-const findKeyByValue = (obj, val) => {};
+
+const findKeyByValue = (show, expectedShow) => {
+  for (const genre in show) {
+    if (expectedShow === show[genre]) {
+      return genre;
+    } else {
+      undefined;
+    }
+  }
+};
 
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire",
 };
-
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"));
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
