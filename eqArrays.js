e05241e5a604b64fe23f8bd26e5e9e1e67d8d42c
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-01-17 01:48:15 +0000

    remove else

--- eqArrays.js
+++ eqArrays.js
@@ -1,26 +1,27 @@
 const assertEqual = function (actual, expected) {
   if (actual !== expected) {
     console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
+  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 };
 
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
-  } else if (arr1 === [] || arr2 === []) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
   }
+
+  if (arr1 === [] || arr2 === []) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
-console.log(eqArrays([], ["1", "2", "3", "4"]));
+eqArrays([], ["1", "2", "3", "4"]);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), true);
