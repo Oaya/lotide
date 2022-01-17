251286841bf1691d2bee3da2e3be672d0bb4cea2
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-01-17 23:14:49 +0000

    implement M & C to headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,23 @@
+const assert = require("chai").assert;
 const head = require("../head");
-const assertEqual = require("../assertEqual");
 
-assertEqual(head([]));
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("return 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("return '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
+
+  it("return undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it("return 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
+
+// assertEqual(head([]));
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
