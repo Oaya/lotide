c142785ccc2dedcc717ac1901364ea8693a83bdc
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-01-17 03:36:26 +0000

    crefactor the function

--- flatten.js
+++ flatten.js
@@ -1,19 +1,23 @@
-const flatten = (array) => {
+const mapThrough = (array) => {
   const newArray = [];
+  for (let i = 0; array.length > i; i++) {
+    if (!Array.isArray(array[i])) {
+      newArray.push(array[i]);
+    }
+    for (let j = 0; array[i].length > j; j++) {
+      newArray.push(array[i][j]);
+    }
+  }
+  return newArray;
+};
+const flatten = (array) => {
+  let newArray = [];
   if (array === []) {
     return false;
   }
 
-  for (let i = 0; array.length > i; i++) {
-    if (Array.isArray(array[i])) {
-      for (let j = 0; array[i].length > j; j++) {
-        newArray.push(array[i][j]);
-      }
-    } else {
-      newArray.push(array[i]);
-    }
-  }
-  console.log(newArray);
+  newArray = mapThrough(array);
   return newArray;
 };
+
 flatten([1, 2, [3, 4], 5, [6]]);
